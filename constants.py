ebdda1071c2db15c8e4c13528e47e81b4860704f
ValentinPro12 <50240752+ValentinPro12@users.noreply.github.com>
2024-01-23 20:29:44 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,14 +1,1 @@
-QMT_URL = "https://tc-qmt3-dev.telecontact.ru/web/"
 
-USERNAME = '52278'
-
-PASSWORD = 'Bh873M'
-
-HEADER_TOKEN = {"Authorization": "Bearer OckctftPCHdbee-o9zbQZ6cKl0SFPGRB"}
-
-CREATE_URL = QMT_URL + 'user/user/delete'
-DELETE_URL = QMT_URL + 'user/user/delete'
-ASSIGN_ROLE_URL = QMT_URL + 'auth/role/assign-role-by-user-id'
-REMOVE_ROLE_URL = QMT_URL + 'auth/role/remove-role-by-user-id'
-ASSIGN_SCALE_URL = QMT_URL + 'user/user-scale/assign-scale-to-user'
-REMOVE_SCALE_URL = QMT_URL + 'user/user-scale/remove-scale-from-user'
